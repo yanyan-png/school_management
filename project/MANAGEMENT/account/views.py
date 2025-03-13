e7b797834b0cd5557b88e01ba75452cd37c7f46c
WrongKillYou <masterkyogre21@gmail.com>
2025-03-13 13:26:30 +0800

    HTML
    Testing

--- project/MANAGEMENT/account/views.py
+++ project/MANAGEMENT/account/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from classroom.models import Badge, Grade, Attendance
+#from django.contrib.auth.decorators import login_required
 
-def login(request):
-    return render(request, 'login.html')
 
 def student_dashboard(request):
     return render(request, 'student/student_dashboard.html')
 
-def student_announcement(request):
-    return render(request, 'student/student_announcement.html')
+
+def login(request):
+    return render(request, 'login.html')
 
